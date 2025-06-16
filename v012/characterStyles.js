493baa07f9a64925e4fe69ea69872c3a663e5db6
YGC-Masa <osaru355@gmail.com>
2025-06-16 20:01:43 +0900

    Update characterStyles.js

--- v012/characterStyles.js
+++ v012/characterStyles.js
@@ -1,8 +1,7 @@
 const characterStyles = {
-  "緋奈":  { fontSize: "1.2em", speed: 30 },  // 明るくテンポよく
-  "藍":    { fontSize: "1.0em", speed: 60 },  // おだやかに、ゆっくり
-  "翠":    { fontSize: "1.1em", speed: 40 },  // 丁寧な口調、中速
-  "こがね": { fontSize: "1.3em", speed: 25 },  // ギャル風に大きめ＆速い
-  "琥珀":  { fontSize: "1.1em", speed: 20 },  // 元気なテンポ、速め
-  "":      { fontSize: "1em", speed: 45 }      // ナレーションやモブ
+  "緋奈": { fontSize: "1.1em", speed: 25 },
+  "藍": { fontSize: "1.0em", speed: 20 },
+  "翠": { fontSize: "1.0em", speed: 22 },
+  "こがね": { fontSize: "1.2em", speed: 24 },
+  "琥珀": { fontSize: "1.3em", speed: 18 }
 };
