e3219b4cdb02e04d4e7c4c0ec01c3a781631ef4b
YGC-Masa <osaru355@gmail.com>
2025-06-17 17:17:37 +0900

    Update characterStyles.js

--- v013/characterStyles.js
+++ v013/characterStyles.js
@@ -1,8 +1,27 @@
-export const characterStyles = {
-  "": { fontSize: "1em", speed: 30 },
-  "緋奈": { fontSize: "1.1em", speed: 25 },
-  "藍": { fontSize: "0.95em", speed: 35 },
-  "翠": { fontSize: "1em", speed: 28 },
-  "こがね": { fontSize: "1.05em", speed: 30 },
-  "琥珀": { fontSize: "1.2em", speed: 20 },
+const characterStyles = {
+  "": {
+    fontSize: "1em",
+    speed: 30
+  },
+  "緋奈": {
+    fontSize: "1.1em",
+    speed: 20
+  },
+  "藍": {
+    fontSize: "1em",
+    speed: 40
+  },
+  "翠": {
+    fontSize: "1em",
+    speed: 25
+  },
+  "こがね": {
+    fontSize: "1.15em",
+    speed: 22
+  },
+  "琥珀": {
+    fontSize: "1.1em",
+    speed: 18
+  }
 };
+
