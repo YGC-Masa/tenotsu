b3615df85b29ee81d9d174098fb9e45432dfc127
YGC-Masa <osaru355@gmail.com>
2025-06-17 17:31:28 +0900

    Update config.js

--- v013/config.js
+++ v013/config.js
@@ -2,5 +2,5 @@ const config = {
   bgPath: "../assets2/bgev/",     // 背景画像フォルダ
   charPath: "../assets2/char/",   // キャラクター画像フォルダ
   bgmPath: "../assets2/bgm/",     // BGMファイルフォルダ
-  scenarioPath: "scenario/"       // シナリオJSONファイルフォルダ
+  scenarioPath: "./scenario/"       // シナリオJSONファイルフォルダ
 };
