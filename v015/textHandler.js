08ca8bbb24aee086cf9fec5b160f855604f92f94
YGC-Masa <osaru355@gmail.com>
2025-06-18 12:27:02 +0900

    Update textHandler.js

--- v015/textHandler.js
+++ v015/textHandler.js
@@ -1,60 +1,18 @@
-// textHandler.js
-// テキスト表示・アニメーション管理モジュール
-import { getCharacterStyle } from './characterStyles.js';
+import { characterStyles } from './characterStyles.js';
+import { characterColors } from './characterColors.js';
 
-export class TextHandler {
-  constructor(nameElement, textElement) {
-    this.nameElement = nameElement;
-    this.textElement = textElement;
+export function setDialogue(nameBox, textBox, name, text) {
+  nameBox.textContent = name || '';
+  textBox.textContent = text || '';
 
-    this.isTyping = false;
-    this.typingTimeout = null;
-    this.currentText = "";
-    this.currentIndex = 0;
-    this.speed = 40;
-  }
+  // 色設定（characterColors.js）
+  nameBox.style.color = characterColors[name] || 'white';
 
-  async setText(name, text) {
-    // 名前に合わせてフォントサイズと速度をセット
-    const style = getCharacterStyle(name.toLowerCase());
-    this.nameElement.style.fontSize = style.fontSize;
-    this.textElement.style.fontSize = style.fontSize;
-    this.speed = style.speed;
-
-    this.nameElement.textContent = name;
-    this.currentText = text;
-    this.currentIndex = 0;
-    this.textElement.textContent = "";
-
-    await this.typeText();
-  }
-
-  typeText() {
-    return new Promise((resolve) => {
-      this.isTyping = true;
-
-      const type = () => {
-        if (this.currentIndex < this.currentText.length) {
-          this.textElement.textContent += this.currentText.charAt(this.currentIndex);
-          this.currentIndex++;
-          this.typingTimeout = setTimeout(type, this.speed);
-        } else {
-          this.isTyping = false;
-          resolve();
-        }
-      };
-
-      type();
-    });
-  }
-
-  skip() {
-    if (this.isTyping) {
-      clearTimeout(this.typingTimeout);
-      this.textElement.textContent = this.currentText;
-      this.isTyping = false;
-      return true; // スキップ成功
-    }
-    return false; // スキップできず（完了している）
+  // スタイル（characterStyles.js）
+  const style = characterStyles[name];
+  if (style && style.font) {
+    textBox.style.font = style.font;
+  } else {
+    textBox.style.font = '1em sans-serif';
   }
 }
