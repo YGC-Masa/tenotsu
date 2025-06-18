6f674973d6b7873c804e6036f835f7d1b75e77f0
YGC-Masa <osaru355@gmail.com>
2025-06-18 13:22:02 +0900

    Delete v015-01/v015 directory

--- v015-01/v015/characterStyles.js
+++
@@ -1,7 +0,0 @@
-export const characterStyles = {
-  "緋奈": { font: "sans-serif" },
-  "藍": { font: "serif" },
-  "翠": { font: "monospace" },
-  "こがね": { font: "cursive" },
-  "琥珀": { font: "sans-serif" }
-};
--- v015-01/v015/config.js
+++
@@ -1,6 +0,0 @@
-export const config = {
-  bgPath: "../assets2/bgev/",
-  charPath: "../assets2/char/",
-  bgmPath: "../assets2/bgm/",
-  scenarioPath: "./scenario/",
-};
