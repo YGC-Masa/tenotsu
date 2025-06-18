a0c9f7209772012f07b10d9b88761c93e3c708e4
YGC-Masa <osaru355@gmail.com>
2025-06-18 13:22:15 +0900

    Delete v015full directory

--- v015full/effectHandler.js
+++
@@ -1,8 +0,0 @@
-export function playEffect(effect, element, callback) {
-  element.classList.remove('fadein');
-  void element.offsetWidth;
-  if (effect === 'fadein') {
-    element.classList.add('fadein');
-  }
-  if (callback) setTimeout(callback, 500);
-}
