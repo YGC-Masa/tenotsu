6e0c177f8373a61f0ff7c7073fd971843f8ee7c8
YGC-Masa <osaru355@gmail.com>
2025-06-18 09:45:09 +0900

    Update characterStyles.js

--- v015/characterStyles.js
+++ v015/characterStyles.js
@@ -1,28 +1,13 @@
+// characterStyles.js
+
 const characterStyles = {
-  "緋奈": {
-    fontSize: "1.1em",
-    textSpeed: 30  // ミリ秒単位で1文字あたりの表示速度
-  },
-  "藍": {
-    fontSize: "1.0em",
-    textSpeed: 40
-  },
-  "翠": {
-    fontSize: "1.0em",
-    textSpeed: 35
-  },
-  "こがね": {
-    fontSize: "1.2em",
-    textSpeed: 25
-  },
-  "琥珀": {
-    fontSize: "1.1em",
-    textSpeed: 20
-  },
-  "ナレーション": {
-    fontSize: "1.0em",
-    textSpeed: 50
-  }
+  hina: { fontSize: "1em", speed: 40 },
+  ai: { fontSize: "1em", speed: 40 },
+  midori: { fontSize: "1em", speed: 40 },
+  kogane: { fontSize: "1em", speed: 40 },
+  kohaku: { fontSize: "1em", speed: 40 }
 };
 
-export default characterStyles;
+export function getCharacterStyle(name) {
+  return characterStyles[name] || { fontSize: "1em", speed: 40 };
+}
