677d59b2d504e8e1081b2a15eca039bc6c2b1b79
YGC-Masa <osaru355@gmail.com>
2025-06-18 13:22:26 +0900

    Delete v015 directory

--- v015/characterStyles.js
+++
@@ -1,7 +0,0 @@
-export const characterStyles = {
-  緋奈: { font: 'bold 1em sans-serif' },
-  藍: { font: 'italic 1em serif' },
-  翠: { font: 'normal 1em "Yu Gothic", sans-serif' },
-  こがね: { font: 'bold 1.1em "Comic Sans MS", cursive' },
-  琥珀: { font: 'normal 1em sans-serif' }
-};
