7da78728dc31763ea4c3ed85e0d8ef965b3fbb30
YGC-Masa <osaru355@gmail.com>
2025-06-18 10:25:17 +0900

    Update config.js

--- v015/config.js
+++ v015/config.js
@@ -1,6 +1,6 @@
 export const config = {
-  bgPath: "../assets2/bgev/",
-  charPath: "../assets2/char/",
-  bgmPath: "../assets2/bgm/",
-  scenarioPath: "scenario/",
+  bgPath: "./assets2/bgev/",
+  charPath: "./assets2/char/",
+  bgmPath: "./assets2/bgm/",
+  scenarioPath: "./scenario/",
 };
