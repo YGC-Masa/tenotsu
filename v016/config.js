08622c30a65739978c16878f6cf811a632fa3f2f
YGC-Masa <osaru355@gmail.com>
2025-06-21 01:24:54 +0900

    Update config.js

--- v016/config.js
+++ v016/config.js
@@ -2,5 +2,7 @@ const config = {
   bgPath: "../assets2/bgev/",
   charPath: "../assets2/char/",
   bgmPath: "../assets2/bgm/",
+  sePath: "../assets2/se/",           // ← 追加
+  voicePath: "../assets2/voice/",     // ← 追加
   scenarioPath: "./scenario/"
 };
