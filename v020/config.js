a286d0f7582d0f02d9ac925bbdf70a1d9fa5e2b9
YGC-Masa <osaru355@gmail.com>
2025-06-21 20:39:50 +0900

    Add files via upload

---
+++ v020/config.js
@@ -0,0 +1,8 @@
+const config = {
+  bgPath: "../assets2/bgev/",
+  charPath: "../assets2/char/",
+  bgmPath: "../assets2/bgm/",
+  sePath: "../assets2/se/",           // 効果音パス
+  voicePath: "../assets2/voice/",     // ボイスパス
+  scenarioPath: "./scenario/"
+};
