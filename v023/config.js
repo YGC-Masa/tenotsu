c53d90c441ee04463b8d73a78142e8d807188c72
YGC-Masa <osaru355@gmail.com>
2025-06-24 21:37:27 +0900

    Update config.js

--- v023/config.js
+++ v023/config.js
@@ -4,6 +4,6 @@ const config = {
   bgmPath: "../assets2/bgm/",
   sePath: "../assets2/se/",           // 効果音パス
   voicePath: "../assets2/voice/",     // ボイスパス
-  scenarioPath: "./scenario/"
+  scenarioPath: "./scenario/",
   menuPath: "./menu/"  // メニューファイル格納先
 };
