a0f571006a01d7a0eb0c2f81571b9ee3cbe5061c
YGC-Masa <osaru355@gmail.com>
2025-07-07 03:46:13 +0900

    Update characterStyles.js

--- v033/characterStyles.js
+++ v033/characterStyles.js
@@ -1,18 +1,37 @@
 const characterStyles = {
-  "": { fontSize: "1em", speed: 40 },
-  "緋奈": { fontSize: "1.2em", speed: 40 },
-  "藍": { fontSize: "1.1em", speed: 50 },
-  "翠": { fontSize: "1.2em", speed: 35 },
-  "こがね": { fontSize: "1.2em", speed: 45 },
-  "琥珀": { fontSize: "1.3em", speed: 25 }
-};
-
-const characterColors = {
-  "店長": "#00ffff",
-  "": "#C0C0C0",
-  "緋奈": "#d3381c",
-  "藍": "#0279e0",
-  "翠": "#02b308",
-  "こがね": "#FFF450",
-  "琥珀": "#F68B1F"
+  "": {
+    fontSize: "1em",
+    speed: 40,
+    color: "#C0C0C0"
+  },
+  "緋奈": {
+    fontSize: "1.2em",
+    speed: 40,
+    color: "#d3381c"
+  },
+  "藍": {
+    fontSize: "1.1em",
+    speed: 50,
+    color: "#0279e0"
+  },
+  "翠": {
+    fontSize: "1.2em",
+    speed: 35,
+    color: "#02b308"
+  },
+  "こがね": {
+    fontSize: "1.2em",
+    speed: 45,
+    color: "#FFF450"
+  },
+  "琥珀": {
+    fontSize: "1.3em",
+    speed: 25,
+    color: "#F68B1F"
+  },
+  "店長": {
+    fontSize: "1em",
+    speed: 40,
+    color: "#00ffff"
+  }
 };
