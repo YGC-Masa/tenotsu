e1708126790fadb10ffdf7630888a4bd1f5bd442
YGC-Masa <osaru355@gmail.com>
2025-07-09 07:08:45 +0900

    Delete v024 directory

--- v024/characterStyles.js
+++
@@ -1,8 +0,0 @@
-const characterStyles = {
-  "": { fontSize: "1em", speed: 40 },
-  "緋奈": { fontSize: "1.2em", speed: 40 },
-  "藍": { fontSize: "1.1em", speed: 50 },
-  "翠": { fontSize: "1.2em", speed: 35 },
-  "こがね": { fontSize: "1.2em", speed: 45 },
-  "琥珀": { fontSize: "1.3em", speed: 25 }
-};
